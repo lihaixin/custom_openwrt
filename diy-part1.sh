c6ffdde073b3068b2557c1c2bae00f6eaf77b594
lihaixin <lihaixin@15099.net>
2021-06-08 18:15:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#git clone https://github.com/lihaixin/custom_package package/my
-sed -i '$a src-git custom_package https://github.com/lihaixin/custom_package' feeds.conf.default
+git clone https://github.com/lihaixin/custom_package package/my
+#sed -i '$a src-git custom_package https://github.com/lihaixin/custom_package' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
